820d7725fc9d25309852d0870e4706a4dcce13df
Tiven <vairtor@gmail.com>
2023-01-25 16:47:21 -0300

    Social networks have been added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import List from "./List";
 import { winnerList1, winnerList2, winnerList3, winnerList4, winnerList5 } from "./data";
 import Money from "./Money";
 import "./index.css";
+import {
+  AiFillTwitterCircle,
+  AiFillGoogleCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
+
 function App() {
   const [collected, setCollected] = useState("");
   const [disableInput, setDisableInput] = useState(false);
   const [reset, setReset] = useState(false);
 
   return (
-    <div className="App bg-dark mt-5">
+    <div className="App bg-dark mt-5 vh-100 d-flex flex-column justify-content-between">
       <div className="container-sm py-4 px-3 mx-auto ">
         <h1 className="text-white">BIG CHRISTMAS WELL</h1>
         <Money
@@ -62,6 +69,55 @@ function App() {
           reset={reset}
         ></List>
       </div>
+      <footer className="bg-dark text-center text-white ">
+        <div className="container p-4 pb-0">
+          <section className="mb-2">
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="https://twitter.com/tivenzss#"
+              target="_blank"
+              role="button"
+            >
+              <AiFillTwitterCircle className="icon" />
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="https://mail.google.com/mail/u/1/?pli=1#inbox"
+              role="button"
+              target="_blank"
+            >
+              <AiFillGoogleCircle />
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="https://www.linkedin.com/in/airtor-stiven-vusanovich-2a6358261/"
+              role="button"
+              target="_blank"
+            >
+              <i className="fab fa-linkedin-in"></i>
+              <AiFillLinkedin />
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="https://github.com/StivenCodess"
+              role="button"
+              target="_blank"
+            >
+              <i className="fab fa-github"></i>
+              <AiFillGithub />
+            </a>
+          </section>
+        </div>
+        <div className="text-center p-3">
+          © 2023 Copyright:
+          <a className="text-white" href="#!" target="_blank">
+            StivenCodess
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
@@ -84,3 +140,15 @@ export default App;
 //? 3ro 15% de lo Collected / cantidad de ganadores
 //? 4to 10% de lo Collected / cantidad de ganadores
 //? 5to 5%  de lo Collected / cantidad de ganadores
+
+/**
+        <div className="container p-4 pb-0">
+        
+        </div>
+        <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+          © 2020 Copyright:
+          <a className="text-white" href="https://mdbootstrap.com/">
+            MDBootstrap.com
+          </a>
+        </div>
+        */
